fe28f4c06e18a93e0cf695179faa4a138aa8b643
huafasheng <huafasheng@outlook.com>
2021-05-17 22:12:04 +0800

    2021.5.17

---
+++ back/how2j/j2se/src/Class/Hero.java
@@ -0,0 +1,17 @@
+package Class;
+
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+    void lengendary(){
+
+    }
+    float getHp(){
+        return this.hp;
+    }
+    void recovery(float blood){
+
+    }
+}
